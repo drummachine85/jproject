a15c9b07b3151721a594c5c53fb7295551b6ac44
drummachine85 <asmevs@gmail.com>
2019-11-12 16:04:20 +0200

    New Commit test

---
+++ src/main/java/edu/jcourse/lesson2/datatypes/repository_test/Repository_test.java
@@ -0,0 +1,8 @@
+package edu.jcourse.lesson2.datatypes.repository_test;
+
+public class Repository_test {
+    public static void main(String[] args) {
+        System.out.println("It is My repository test!!!");
+        System.out.println("Works properly.");
+    }
+}
